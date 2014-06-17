6c7c5bd7f7939566ee53c4751d057801ebfc0e6a
ss1891 <simon.salloum@hotmail.se>
2014-06-17 13:40:09 +0200

    runfile now writes to output files without overwriting existing outputs

--- bin/runfile.sh
+++ bin/runfile.sh
@@ -1,4 +1,4 @@
-{ time ./micro-broadcast $1 $2 ; } 2> ../output/micro-broadcast.txt
-{ time ./micro-multiplex $1 $2; } 2> ../output/micro-multiplex.txt
-{ time ./micro-ping $1 $2; } 2> ../output/micro-ping.txt
-{ time ./micro-pingpong $1 $2; } 2> ../output/micro-pingpong.txt
+{ time ./micro-broadcast $1 $2 ; } 2>> ../output/micro-broadcast.txt
+{ time ./micro-multiplex $1 $2; } 2>> ../output/micro-multiplex.txt
+{ time ./micro-ping $1 $2; } 2>> ../output/micro-ping.txt
+{ time ./micro-pingpong $1 $2; } 2>> ../output/micro-pingpong.txt
