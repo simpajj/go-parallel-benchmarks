893a9cead1808a6441ea56eaa6103943d23d989a
ss1891 <simon.salloum@hotmail.se>
2014-06-23 13:44:56 +0200

    matrix multiplication code added

---
+++ src/seq-matrixmult/seq-matrixmult.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+const int range = 5;
+
+int main() {
+	srand(time(NULL));
+	enum { ROWS = 3, COLS = 3 };
+	int c, d, k, sum = 0;
+	int first[10][10], second[10][10], result[10][10];
+
+	// Populate first matrix
+	for (c = 0; c < ROWS; c++) 
+		for (d = 0; d < COLS; d++)
+			first[c][d] = rand() % range;
+
+	// Populate second matrix
+	for (c = 0; c < ROWS; c++)
+		for (d = 0; d < COLS; d++)
+			second[c][d] = rand() % range;
+
+	// Multiply
+	for (c = 0; c < ROWS; c++) {
+		for (d = 0; d < COLS; d++) {
+			for (k = 0; k < ROWS; k++) {
+				sum = sum + first[c][k] * second[k][d];
+			}
+
+			result[c][d] = sum;
+			sum = 0;
+		}
+	}
+
+	printf("Product: \n");
+
+	for (c = 0; c < ROWS; c++) {
+		for (d = 0; d < COLS; d++) {
+			printf("%d\t", result[c][d]);
+		}
+		printf("\n");
+	} 
+
+	return 0;
+}
---
+++ src/seq-matrixmult/seq-matrixmult.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const RANGE = 5
+const (
+	ROWS = 3
+	COLS = 3
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var sum int = 0
+
+	/* Create matrices */
+	first := make([][]int, ROWS)
+	second := make([][]int, ROWS)
+	result := make([][]int, ROWS)
+	for i := range first {
+		first[i] = make([]int, COLS)
+		second[i] = make([]int, COLS)
+		result[i] = make([]int, COLS)
+	}
+
+	/* Populate matrices */
+	for c := 0; c < ROWS; c++ {
+		for d := 0; d < COLS; d++ {
+			first[c][d] = rand.Intn(RANGE - 0)
+		}
+	}
+
+	for c := 0; c < ROWS; c++ {
+		for d := 0; d < COLS; d++ {
+			second[c][d] = rand.Intn(RANGE - 0)
+		}
+	}
+
+	/* Multiply matrices */
+	for c := 0; c < ROWS; c++ {
+		for d := 0; d < COLS; d++ {
+			for k := 0; k < ROWS; k++ {
+				sum = sum + first[c][k]*second[k][d]
+			}
+
+			result[c][d] = sum
+			sum = 0
+		}
+	}
+
+	fmt.Println("Product: \n")
+	for c := 0; c < ROWS; c++ {
+		for d := 0; d < COLS; d++ {
+			fmt.Printf("%d\t", result[c][d])
+		}
+		fmt.Printf("\n")
+	}
+}
