d864cc30265f21fff5e432dbb5d989385f18e0f5
ss1891 <simon.salloum@hotmail.se>
2014-06-30 17:41:06 +0200

    cross compiled go code to 32-bit

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 # --------------------------------------
 
 # If needed
+export GOOS=linux
+export GOARCH=386
 export GOPATH=$GOPATH
 export GOBIN=$GOPATH/bin
 
@@ -16,11 +18,11 @@ go install src/micro-ping/micro-ping.go
 go install src/micro-pingpong/micro-pingpong.go
 
 # C files
-gcc -m32 src/seq-binarysearch/seq-binarysearch.c -o bin/seq-binarysearch-c
-gcc -m32 src/seq-bubblesort/seq-bubblesort.c -o bin/seq-bubblesort-c
-gcc -m32 src/seq-gcd-lcm/seq-gcd-lcm.c -o bin/seq-gcd-lcm-c
-gcc -m32 src/seq-matrixmult/seq-matrixmult.c -o bin/seq-matrixmult-c
-gcc-4.9 -fopenmp -m32 src/micro-broadcast/micro-broadcast.c -o bin/micro-broadcast-c
-gcc-4.9 -fopenmp -m32 src/micro-multiplex/micro-multiplex.c -o bin/micro-multiplex-c
-gcc-4.9 -fopenmp -m32 src/micro-ping/micro-ping.c -o bin/micro-ping-c
-gcc-4.9 -fopenmp -m32 src/micro-pingpong/micro-pingpong.c -o bin/micro-pingpong-c
+gcc src/seq-binarysearch/seq-binarysearch.c -o bin/seq-binarysearch-c
+gcc src/seq-bubblesort/seq-bubblesort.c -o bin/seq-bubblesort-c
+gcc src/seq-gcd-lcm/seq-gcd-lcm.c -o bin/seq-gcd-lcm-c
+gcc src/seq-matrixmult/seq-matrixmult.c -o bin/seq-matrixmult-c
+gcc-4.9 -fopenmp src/micro-broadcast/micro-broadcast.c -o bin/micro-broadcast-c
+gcc-4.9 -fopenmp src/micro-multiplex/micro-multiplex.c -o bin/micro-multiplex-c
+gcc-4.9 -fopenmp src/micro-ping/micro-ping.c -o bin/micro-ping-c
+gcc-4.9 -fopenmp src/micro-pingpong/micro-pingpong.c -o bin/micro-pingpong-c
