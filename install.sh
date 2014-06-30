9bb7848ea92252ebe6c1b6c8220fb2b43d819d08
ss1891 <simon.salloum@hotmail.se>
2014-06-30 21:28:53 +0200

    32-bit executables

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ go install src/micro-ping/micro-ping.go
 go install src/micro-pingpong/micro-pingpong.go
 
 # C files
-gcc src/seq-binarysearch/seq-binarysearch.c -o bin/seq-binarysearch-c
-gcc src/seq-bubblesort/seq-bubblesort.c -o bin/seq-bubblesort-c
-gcc src/seq-gcd-lcm/seq-gcd-lcm.c -o bin/seq-gcd-lcm-c
-gcc src/seq-matrixmult/seq-matrixmult.c -o bin/seq-matrixmult-c
-gcc-4.9 -fopenmp src/micro-broadcast/micro-broadcast.c -o bin/micro-broadcast-c
-gcc-4.9 -fopenmp src/micro-multiplex/micro-multiplex.c -o bin/micro-multiplex-c
-gcc-4.9 -fopenmp src/micro-ping/micro-ping.c -o bin/micro-ping-c
-gcc-4.9 -fopenmp src/micro-pingpong/micro-pingpong.c -o bin/micro-pingpong-c
+gcc src/seq-binarysearch/seq-binarysearch.c -m32 -o bin/seq-binarysearch-c
+gcc src/seq-bubblesort/seq-bubblesort.c -m32 -o bin/seq-bubblesort-c
+gcc src/seq-gcd-lcm/seq-gcd-lcm.c -m32 -o bin/seq-gcd-lcm-c
+gcc src/seq-matrixmult/seq-matrixmult.c -m32 -o bin/seq-matrixmult-c
+gcc-4.9 -fopenmp src/micro-broadcast/micro-broadcast.c -m32 -o bin/micro-broadcast-c
+gcc-4.9 -fopenmp src/micro-multiplex/micro-multiplex.c -m32 -o bin/micro-multiplex-c
+gcc-4.9 -fopenmp src/micro-ping/micro-ping.c -m32 -o bin/micro-ping-c
+gcc-4.9 -fopenmp src/micro-pingpong/micro-pingpong.c -m32 -o bin/micro-pingpong-c
