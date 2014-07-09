73d683781202ff6c7f1a2432806cd9f31a01da9d
ss1891 <simon.salloum@hotmail.se>
2014-07-09 23:33:49 +0200

    fixed index issues in dotprod.go

--- src/component-dotprod/component-dotprod.go
+++ src/component-dotprod/component-dotprod.go
@@ -7,10 +7,8 @@ import (
 	"strconv"
 )
 
-const n = 100
-
 func dotprod(A, B []float64, result float64) float64 {
-	for i := 0; i < n; i++ {
+	for i := range A {
 		result += A[i] * B[i]
 	}
 	return result
@@ -25,7 +23,7 @@ func main() {
 
 	result := 0.0
 	for i := 0; i <= N; i++ {
-		for j := 0; j < n; j++ {
+		for j := range A {
 			A[j] = float64(j) * 1.0
 			B[j] = float64(j) * 2.0
 			go dotprod(A, B, result)
