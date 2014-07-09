78921efd7fa15ee49105525b569779d9e7df7e20
ss1891 <simon.salloum@hotmail.se>
2014-07-09 21:11:02 +0200

    -o3 optimization

--- ccompile.sh
+++ ccompile.sh
@@ -1,13 +1,13 @@
-gcc src/seq-binarysearch/seq-binarysearch.c -O3 bin/seq-binarysearch-c
-gcc src/seq-bubblesort/seq-bubblesort.c -O3 bin/seq-bubblesort-c
-gcc src/seq-gcd-lcm/seq-gcd-lcm.c -O3 bin/seq-gcd-lcm-c
-gcc src/seq-matrixmult/seq-matrixmult.c -O3 bin/seq-matrixmult-c
-gcc -fopenmp src/micro-broadcast/micro-broadcast.c -O3 bin/micro-broadcast-c
-gcc -fopenmp src/micro-multiplex/micro-multiplex.c -O3 bin/micro-multiplex-c
-gcc -fopenmp src/micro-ping/micro-ping.c -O3 bin/micro-ping-c
-gcc -fopenmp src/micro-pingpong/micro-pingpong.c -O3 bin/micro-pingpong-c
-gcc -fopenmp src/component-dotprod/component-dotprod.c -O3 bin/component-dotprod-c
-gcc -fopenmp src/component-factorial/component-factorial.c -O3 bin/component-factorial-c
-gcc -fopenmp src/component-pi/component-pi.c -O3 bin/component-pi-c
-gcc -fopenmp src/component-primefilter/component-primefilter.c -O3 bin/component-primefilter-c
-gcc -fopenmp src/component-quicksort/component-quicksort.c -O3 bin/component-quicksort-c
+gcc -O3 src/seq-binarysearch/seq-binarysearch.c -o bin/seq-binarysearch-c
+gcc -O3 src/seq-bubblesort/seq-bubblesort.c -o bin/seq-bubblesort-c
+gcc -O3 src/seq-gcd-lcm/seq-gcd-lcm.c -o bin/seq-gcd-lcm-c
+gcc -O3 src/seq-matrixmult/seq-matrixmult.c -o bin/seq-matrixmult-c
+gcc -fopenmp -O3 src/micro-broadcast/micro-broadcast.c -o bin/micro-broadcast-c
+gcc -fopenmp -O3 src/micro-multiplex/micro-multiplex.c -o bin/micro-multiplex-c
+gcc -fopenmp -O3 src/micro-ping/micro-ping.c -o bin/micro-ping-c
+gcc -fopenmp -O3 src/micro-pingpong/micro-pingpong.c -o bin/micro-pingpong-c
+gcc -fopenmp -O3 src/component-dotprod/component-dotprod.c -o bin/component-dotprod-c
+gcc -fopenmp -O3 src/component-factorial/component-factorial.c -o bin/component-factorial-c
+gcc -fopenmp -O3 src/component-pi/component-pi.c -o bin/component-pi-c
+gcc -fopenmp -O3 src/component-primefilter/component-primefilter.c -o bin/component-primefilter-c
+gcc -fopenmp -O3 src/component-quicksort/component-quicksort.c -o bin/component-quicksort-c
