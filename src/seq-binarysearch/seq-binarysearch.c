6961fc81cc094c39085f508014e8c98e2b1f28fc
ss1891 <simon.salloum@hotmail.se>
2014-07-10 12:48:51 +0200

    refactored binary search code

--- src/seq-binarysearch/seq-binarysearch.c
+++ src/seq-binarysearch/seq-binarysearch.c
@@ -4,32 +4,40 @@
 
 #define numberOfElements 100
 
+int binarysearch(int []);
+
+int binarysearch(int array[]) {
+	int c, 
+			first = 0, 
+			last = numberOfElements - 1, 
+			middle = (first + last)/2, 
+			search = 13;
+
+	for (c = 0; c < numberOfElements; c++)
+		array[c] = rand() % 101;
+
+	while (first <= last) {
+		if (array[middle] < search)
+			first = middle + 1;
+		else if (array[middle] == search) {
+			search = array[middle];
+			return search;
+		}
+		else
+			last = middle - 1;
+
+		middle = (first + last)/2;
+	}
+	if (first > last)
+		return 0;
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	const int N = atoi(argv[1]);
-	int i, c, first, last, middle, search = 13, array[numberOfElements];
+	int i, c, array[numberOfElements];
 
-	for (i = 0; i <= N; i++) {
-		for (c = 0; c < numberOfElements; c++)
-			array[c] = rand() % 101;
-
-		first = 0;
-		last = numberOfElements - 1;
-		middle = (first + last)/2;
-
-		while (first <= last) {
-			if (array[middle] < search)
-				first = middle + 1;
-			else if (array[middle] == search) {
-				return 0;
-			}
-			else
-				last = middle - 1;
-
-			middle = (first + last)/2;
-		}
-		if (first > last)
-			return 0;
-	}
-	return 0;
+	for (i = 0; i <= N; i++)
+		binarysearch(array);
 }
--- src/seq-binarysearch/seq-binarysearch.go
+++ src/seq-binarysearch/seq-binarysearch.go
@@ -10,36 +10,41 @@ import (
 
 const number_of_elements = 100
 
+func binarysearch(array []int) int {
+	search := 13
+	for c := range array {
+		array[c] = rand.Intn(number_of_elements - 0)
+	}
+
+	first := 0
+	last := number_of_elements - 1
+	middle := (first + last) / 2
+
+	for first <= last {
+		if array[middle] < search {
+			first = middle + 1
+		} else if array[middle] == search {
+			search = array[middle]
+			break
+		} else {
+			last = middle - 1
+		}
+
+		middle = (first + last) / 2
+	}
+
+	if first > last {
+		return -1
+	}
+	return search
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	N, _ := strconv.Atoi(os.Args[1])
-	var first, last, middle int
-	search := 13
 	array := make([]int, number_of_elements)
 
 	for i := 0; i <= N; i++ {
-		for c := range array {
-			array[c] = rand.Intn(number_of_elements - 0)
-		}
-
-		first = 0
-		last = number_of_elements - 1
-		middle = (first + last) / 2
-
-		for first <= last {
-			if array[middle] < search {
-				first = middle + 1
-			} else if array[middle] == search {
-				break
-			} else {
-				last = middle - 1
-			}
-
-			middle = (first + last) / 2
-		}
-
-		if first > last {
-			break
-		}
+		binarysearch(array)
 	}
 }
